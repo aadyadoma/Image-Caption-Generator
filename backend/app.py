c3584a25805394224b96d81399acf3a5d61a2f12
aadyadoma <aadya.doma@gmail.com>
2025-04-16 17:22:34 -0500

    made it work

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from transformers import BlipProcessor, BlipForConditionalGeneration
 from PIL import Image
 import io
+from flask_cors import CORS
+
+
 
 app = Flask(__name__)
+CORS(app)
 processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
 model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
@@ -17,6 +21,13 @@ def caption():
     caption = processor.decode(out[0], skip_special_tokens=True)
     
     return jsonify({"caption": caption})
+    # print("received request")
+    # return "This is a test caption"
+    # return jsonify({"caption": "meow"})
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=False, use_reloader=False)
+
--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,25 @@
 async function uploadImage() {
-    const input = document.getElementById("imageInput");
-    const file = input.files[0];
-    const formData = new FormData();
-    formData.append("image", file);
-  
+  const input = document.getElementById("imageInput");
+  const file = input.files[0];
+  const formData = new FormData();
+  formData.append("image", file);
+
+  try {
     const res = await fetch("http://localhost:5000/caption", {
       method: "POST",
       body: formData
     });
-  
-    const data = await res.json();
-    document.getElementById("caption").innerText = data.caption;
+
+    // const text = await res.text();
+    const text = await res.json();
+    console.log("Status:", res.status);
+    console.log("Response text:", text);
+
+    if (!res.ok) throw new Error(`Server error: ${res.status}`);
+
+    document.getElementById("caption").innerText = text.caption;
+  } catch (err) {
+    console.error("error fetching caption:", err);
+    document.getElementById("caption").innerText = "failed to get caption. :3 ";
   }
-  
+}
